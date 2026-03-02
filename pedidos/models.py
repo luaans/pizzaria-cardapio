c4e80763fd947d08161b29e7cd6b07a9a5f854f9
luaans <luanssilvasanttos@gmail.com>
2026-03-02 12:45:02 -0300

    Cria modelos pizza , sabores e acompanhamento

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Criando o modelo para pizza 
+
+class Pizza(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nome
+    
+class Sabores(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    disponivel = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+class Acompanhamentos(models.Model):  
+    nome = models.CharField(max_length=100) 
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=6, decimal_places=2)
+    disponivel = models.BooleanField(default=True) 
+    
+    def __str__(self):
+        return self.nome
