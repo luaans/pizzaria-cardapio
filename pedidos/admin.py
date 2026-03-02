6195ad94ce7885a5eda22cd8f031bc23ecc92467
luaans <luanssilvasanttos@gmail.com>
2026-03-02 12:48:19 -0300

    Configura admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+# Criando o admin para os modelos de pizza , sabores e acompanhamentos
+from . models import Pizza, Sabores, Acompanhamentos
+
+admin.site.register(Pizza)
+admin.site.register(Sabores)
+admin.site.register(Acompanhamentos)
